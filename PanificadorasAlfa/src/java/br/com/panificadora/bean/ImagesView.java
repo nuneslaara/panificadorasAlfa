6464614673d5e74716eaf469ddb5497718c75d69
thayana <thayana694@gmail.com>
2016-04-29 15:16:32 -0300

    sexto commit

---
+++ PanificadorasAlfa/src/java/br/com/panificadora/bean/ImagesView.java
@@ -0,0 +1,23 @@
+package br.com.panificadora.bean;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 10; i++) {
+            images.add("pao" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
